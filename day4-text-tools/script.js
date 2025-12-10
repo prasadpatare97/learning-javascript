4c20a6181d8ec6c8ab0c4046bbbb35b573d4fea7
prasadpatare97 <prasadpatare97@gmail.com>
2025-12-10 11:50:02 +0530

    added word counter

--- day4-text-tools/script.js
+++ day4-text-tools/script.js
@@ -1,6 +1,7 @@
 const textArea=document.getElementById("textArea");
 const charsCount=document.getElementById("chars");
 const charsWoSpaces=document.getElementById("charsWoSpaces");
+const wordCount=document.getElementById("wordCountResult");
 textArea.addEventListener("input",()=>{
     let chars=0;
     let string1=textArea.value;
@@ -10,4 +11,12 @@ textArea.addEventListener("input",()=>{
     let chars2=stringArr2.length;
     charsCount.textContent=`characters: ${chars}`;
     charsWoSpaces.textContent=`characters (no spaces): ${chars2}`;
+    let string2=string1.replace(/\r?\n/g, " ");
+   
+    let stringArr3=string2.split(" ");
+    let stringArr4=stringArr3.filter(element=>!((element=="")||(element==" ")||(element==".")||(element==",")||(element=="!")||(element=="-")||(element=="?")||(element=="/")));
+    let words=stringArr4.length;
+    wordCount.textContent=`Words: ${words}`;
 })
+
+//word counter 
